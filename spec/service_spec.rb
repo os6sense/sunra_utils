72bdfba0f4b19d2842f177927cd5da7472e5d082
os6sense <leej@sowhatresearch.com>
2014-09-24 18:28:06 +0100

    Service specs, bare

--- spec/service_spec.rb
+++ spec/service_spec.rb
@@ -1,3 +1,26 @@
-
 require_relative 'spec_helper'
+require_relative '../lib/sunra_utils/service'
+
+describe Service do
+
+  it 'displays starts a service' do
+
+  end
+
+  it 'stops a service' do
+  end
+
+  it 'provides a monitor which does not daemonise the service' do
+
+  end
+
+  it 'restarts the service' do
+  end
+
+  it 'displays the service status' do
+  end
+
+
+
+end
 
